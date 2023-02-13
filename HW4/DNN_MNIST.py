9dfe20139e8c0d781f57c7518a4b7c04eaee41ff
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2023-02-13 03:12:39 -0500

    Add unpack function

--- HW4/DNN_MNIST.py
+++ HW4/DNN_MNIST.py
@@ -69,7 +69,60 @@ class DNN:
         Ws.append(W)
         b = 0.01 * np.ones(self.no_outputs)
         bs.append(b)
+        # "Pack" all the weight matrices and bias vectors into long one parameter "vector".
+        weights = np.hstack([ W.flatten() for W in Ws ] + [ b.flatten() for b in bs ])
+        return weights
+
+    def unpack(self,weights,no_hidden_layers,no_hidden_units):
+        # Unpack arguments
+        Ws = []
+
+        # Weight matrices
+        start = 0
+        end = self.no_inputs*no_hidden_units[0] #NUM_INPUT*NUM_HIDDEN[0]
+        W = weights[start:end]
+        Ws.append(W)
+
+        # Unpack the weight matrices as vectors
+        for i in range(no_hidden_layers - 1):
+            start = end
+            end = end + no_hidden_units[i]*no_hidden_units[i+1]
+            W = weights[start:end]
+            Ws.append(W)
+
+        start = end
+        end = end + no_hidden_units[-1]*self.no_outputs
+        W = weights[start:end]
+        Ws.append(W)
+
+        # Reshape the weight "vectors" into proper matrices
+        Ws[0] = Ws[0].reshape(self.no_inputs,no_hidden_units[0])
+        for i in range(1, no_hidden_layers ):
+            # Convert from vectors into matrices
+            Ws[i] = Ws[i].reshape(no_hidden_units[i-1], no_hidden_units[i])
+        Ws[-1] = Ws[-1].reshape(no_hidden_units[-1],self.no_outputs)
+
+        # Bias terms
+        bs = []
+        start = end
+        end = end + no_hidden_units[0]
+        b = weights[start:end]
+        bs.append(b)
+
+        for i in range(no_hidden_layers - 1):
+            start = end
+            end = end + no_hidden_units[i+1]
+            b = weights[start:end]
+            bs.append(b)
+
+        start = end
+        end = end + self.no_outputs
+        b = weights[start:end]
+        bs.append(b)
+
         return Ws, bs
+        
+
 
     def ReLU(self,z):
         # Relu activation function 
@@ -79,16 +132,28 @@ class DNN:
         # softmax activation 
         exp = np.exp(z) 
         return exp /np.sum(exp,axis=1,keepdims=1)
+    # def CE_loss(self,X,y,Ws,bs,alpha,regularize=True):
+    #     # Cross entropy loss 
+    #     n = y.shape[0]
+    #     # y_tilde = self.predict(X,y,w)
+    #     y_tilde = self.forward_propagation(X,Ws,bs)
+    #     if regularize:
+    #         w_temp = w[:-1,:] #to not reg the bias 
+    #         reg = np.sum(w_temp.T@w_temp,axis=0)
+    #         return -(1/n)*np.sum(np.sum(y*np.log(y_tilde),axis=0)+ 0.5*alpha*reg)
+    #     else:
+    #         return -(1/n)*np.sum(np.sum(y*np.log(y_tilde),axis=0))
 
-    def forward_propagation(self,X,Ws,bs):
+    def forward_propagation(self,X,weights,no_hidden_layers,no_hidden_units):
         # ReLU activation is used for every layer except the last layer 
         # For last layer it is soft max activation 
+        Ws,bs = self.unpack(weights,no_hidden_layers,no_hidden_units)
         h = X.T 
         for i,(w,b) in enumerate(zip(Ws,bs)):
-            # print(h.shape)
-            # print(w.shape)
-            # print(b.shape)
-            z = h@w+b#[:,None]  # make sure to transpose b to be a column vector 
+            print(h.shape)
+            print(w.shape)
+            print(b.shape)
+            z = h@w+b
             if i != len(Ws)-1:
                 h = self.ReLU(z) 
             else:
@@ -101,15 +166,16 @@ class DNN:
         # this is where we update the weights based the gradient 
         return 
 
-    def train_batch(self,X,y,Ws,bs,alpha,epsilon,n,n_,regularize=True):
+    def train_batch(self,X,y,weights,no_hidden_layers,no_hidden_units,alpha,epsilon,n,n_,regularize=True):
         # Split the batch 
         X = X[:,n_:n+n_]
         y = y[n_:n+n_]
         # forward propagation 
-        y_hat = self.forward_propagation(X,Ws,bs)
+        y_hat = self.forward_propagation(X,weights,no_hidden_layers,no_hidden_units)
         # backward propagation 
+
         n_+=n
-        return Ws,bs, n_
+        return weights, n_
 
     def tuning(self):
         # this function finds out the best hyper parameter set 
@@ -119,25 +185,16 @@ class DNN:
         err = np.inf #initial error
         for h in self.H: # for each hyper parameter set 
             no_hidden_layers,no_hidden_units,n,epsilon,epochs,alpha = h 
-            no_hidden_units = int(no_hidden_layers)*[int(no_hidden_units)]
-            Ws,bs = self.initWeightsAndBiases(int(no_hidden_layers),no_hidden_units)
-            # print("shape",self.forward_propagation(X_tr,Ws,bs).shape)
-            # bs = arr[:,None]
-            # print(Ws[0],bs[0][:,None])
-            # input()
-            # for i,(w,b) in enumerate(zip(Ws,bs)):
-            #     print(i,w,b)
-            #     input()
-            # n = int(n) 
-            # epochs = int(epochs) 
-            # for epoch in range(epochs):
-            #     n_ = 0 
-            #     while n_ < len(ytr):
-            #         # forward propagation 
-            #         self.forward_propagation()
-
-            #         # backward propagation 
-            #         w,n_ = self.train_batch(X_tr,ytr,w,alpha,epsilon,n,n_,regularize=False) #Train on the batch
+            no_hidden_layers = int(no_hidden_layers)
+            no_hidden_units = no_hidden_layers*[int(no_hidden_units)]
+            weights = self.initWeightsAndBiases(int(no_hidden_layers),no_hidden_units)
+            # print("shape",self.forward_propagation(X_tr,Ws,bs)[0])
+            n = int(n) 
+            epochs = int(epochs) 
+            for epoch in range(epochs):
+                n_ = 0 
+                while n_ < len(ytr):
+                    weights,n_ = self.train_batch(X_tr,ytr,weights,no_hidden_layers,no_hidden_units,alpha,epsilon,n,n_,regularize=False) #Train on the batch
         
 
 
